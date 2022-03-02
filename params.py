024c4bdf604a80a4887ab49ddf81f3264c7fad35
Muhammad Hanan Asghar <44013285+MuhammadHananAsghar@users.noreply.github.com>
2022-03-02 10:05:35 +0500

    Update params.py

--- params.py
+++ params.py
@@ -2,11 +2,16 @@ import os
 
 if os.path.exists("./cropped")==False:
     os.makedirs("./cropped")
-
+ 
+# Alpha value for the triplet loss function
 ALPHA = 0.5
-THRESHOLD = 0.5
+# Image size for the network
 IMAGE_SIZE= 96
+# Freezing inceptionNet layers
 LAYERS_TO_FREEZE= 60
+# No of epochs in training Model
 NUM_EPOCHS= 1
+# No of Steps per Epochs
 STEPS_PER_EPOCH= 1
+# Batch Size for training
 BATCH_SIZE= 64
